f942bc277af514516f1957cc7514fcc893671aa4
gemmaro <gemmaro.dev@gmail.com>
2020-12-06 21:03:07 +0900

    copy from draft

---
+++ README.md
@@ -0,0 +1,9 @@
+# heck CLI
+
+```shell-session
+$ echo 'This is a heck CLI' | heck
+this-is-a-heck-cli
+
+$ echo 'This is a heck CLI' | heck -t shouty-snake
+THIS_IS_A_HECK_CLI
+```
---
+++ src/bin/heck.rs
@@ -0,0 +1,52 @@
+use {
+    heck::{CamelCase, KebabCase, MixedCase, ShoutySnakeCase, SnakeCase, TitleCase},
+    std::io::{stdin, Read},
+    structopt::StructOpt,
+    strum_macros::EnumString,
+};
+
+fn main() {
+    let input_string = {
+        let mut buf = String::new();
+        stdin().read_to_string(&mut buf).unwrap();
+
+        buf
+    };
+
+    let output_string = match Opt::from_args()
+        .target_case
+        .unwrap_or(TargetCase::default())
+    {
+        TargetCase::Camel => input_string.to_camel_case(),
+        TargetCase::Kebab => input_string.to_kebab_case(),
+        TargetCase::Mixed => input_string.to_mixed_case(),
+        TargetCase::ShoutySnake => input_string.to_shouty_snake_case(),
+        TargetCase::Snake => input_string.to_snake_case(),
+        TargetCase::Title => input_string.to_title_case(),
+    };
+
+    println!("{}", output_string);
+}
+
+#[derive(Debug, StructOpt)]
+struct Opt {
+    #[structopt(short = "t", long = "target-case")]
+    target_case: Option<TargetCase>,
+}
+
+#[derive(Debug, EnumString)]
+#[strum(serialize_all = "kebab_case")]
+enum TargetCase {
+    Camel,
+    Kebab,
+    Mixed,
+    ShoutySnake,
+    Snake,
+    Title,
+}
+
+impl Default for TargetCase {
+    fn default() -> Self {
+        Self::Kebab
+    }
+}
